73f76f2c93c49beaa7b7465402cede220d67363d
Darx <mcrivabode@gmail.com>
2022-11-28 14:53:54 -0800

    create model files, setup index.js

---
+++ models/index.js
@@ -0,0 +1,6 @@
+// require User and Thought models from their respective files
+const User = require('./User');
+const Thought = require('./Thought');
+
+// export models for use elsewhere
+module.exports = { User, Thought };
